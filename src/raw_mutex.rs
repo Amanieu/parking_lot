dea3398851a36cf297808b62adad0f90f63c727c
Linus Färnstrand <faern@faern.net>
2019-09-24 23:34:54 +0200

    Reduce scope of unsafe block and add SAFETY doc

--- src/raw_mutex.rs
+++ src/raw_mutex.rs
@@ -220,37 +220,41 @@ impl RawMutex {
             }
 
             // Park our thread until we are woken up by an unlock
-            unsafe {
-                let addr = self as *const _ as usize;
-                let validate = || self.state.load(Ordering::Relaxed) == LOCKED_BIT | PARKED_BIT;
-                let before_sleep = || {};
-                let timed_out = |_, was_last_thread| {
-                    // Clear the parked bit if we were the last parked thread
-                    if was_last_thread {
-                        self.state.fetch_and(!PARKED_BIT, Ordering::Relaxed);
-                    }
-                };
-                match parking_lot_core::park(
+            let addr = self as *const _ as usize;
+            let validate = || self.state.load(Ordering::Relaxed) == LOCKED_BIT | PARKED_BIT;
+            let before_sleep = || {};
+            let timed_out = |_, was_last_thread| {
+                // Clear the parked bit if we were the last parked thread
+                if was_last_thread {
+                    self.state.fetch_and(!PARKED_BIT, Ordering::Relaxed);
+                }
+            };
+            // SAFETY:
+            //   * `addr` is an address we control.
+            //   * `validate`/`timed_out` does not panic or call into any function of `parking_lot`.
+            //   * `before_sleep` does not call `park`, nor does it panic.
+            match unsafe {
+                parking_lot_core::park(
                     addr,
                     validate,
                     before_sleep,
                     timed_out,
                     DEFAULT_PARK_TOKEN,
                     timeout,
-                ) {
-                    // The thread that unparked us passed the lock on to us
-                    // directly without unlocking it.
-                    ParkResult::Unparked(TOKEN_HANDOFF) => return true,
+                )
+            } {
+                // The thread that unparked us passed the lock on to us
+                // directly without unlocking it.
+                ParkResult::Unparked(TOKEN_HANDOFF) => return true,
 
-                    // We were unparked normally, try acquiring the lock again
-                    ParkResult::Unparked(_) => (),
+                // We were unparked normally, try acquiring the lock again
+                ParkResult::Unparked(_) => (),
 
-                    // The validation function failed, try locking again
-                    ParkResult::Invalid => (),
+                // The validation function failed, try locking again
+                ParkResult::Invalid => (),
 
-                    // Timeout expired
-                    ParkResult::TimedOut => return false,
-                }
+                // Timeout expired
+                ParkResult::TimedOut => return false,
             }
 
             // Loop back and try locking again
@@ -263,29 +267,32 @@ impl RawMutex {
     fn unlock_slow(&self, force_fair: bool) {
         // Unpark one thread and leave the parked bit set if there might
         // still be parked threads on this address.
-        unsafe {
-            let addr = self as *const _ as usize;
-            let callback = |result: UnparkResult| {
-                // If we are using a fair unlock then we should keep the
-                // mutex locked and hand it off to the unparked thread.
-                if result.unparked_threads != 0 && (force_fair || result.be_fair) {
-                    // Clear the parked bit if there are no more parked
-                    // threads.
-                    if !result.have_more_threads {
-                        self.state.store(LOCKED_BIT, Ordering::Relaxed);
-                    }
-                    return TOKEN_HANDOFF;
+        let addr = self as *const _ as usize;
+        let callback = |result: UnparkResult| {
+            // If we are using a fair unlock then we should keep the
+            // mutex locked and hand it off to the unparked thread.
+            if result.unparked_threads != 0 && (force_fair || result.be_fair) {
+                // Clear the parked bit if there are no more parked
+                // threads.
+                if !result.have_more_threads {
+                    self.state.store(LOCKED_BIT, Ordering::Relaxed);
                 }
+                return TOKEN_HANDOFF;
+            }
 
-                // Clear the locked bit, and the parked bit as well if there
-                // are no more parked threads.
-                if result.have_more_threads {
-                    self.state.store(PARKED_BIT, Ordering::Release);
-                } else {
-                    self.state.store(0, Ordering::Release);
-                }
-                TOKEN_NORMAL
-            };
+            // Clear the locked bit, and the parked bit as well if there
+            // are no more parked threads.
+            if result.have_more_threads {
+                self.state.store(PARKED_BIT, Ordering::Release);
+            } else {
+                self.state.store(0, Ordering::Release);
+            }
+            TOKEN_NORMAL
+        };
+        // SAFETY:
+        //   * `addr` is an address we control.
+        //   * `callback` does not panic or call into any function of `parking_lot`.
+        unsafe {
             parking_lot_core::unpark_one(addr, callback);
         }
     }
