a5d61df694d42ec4c311fd9029ea96b3fa11d788
Linus Färnstrand <faern@faern.net>
2019-10-03 22:32:00 +0200

    Vendor MaybeUninit from libcore for old Rust versions

--- core/build.rs
+++ core/build.rs
@@ -3,4 +3,7 @@ fn main() {
     if cfg.probe_rustc_version(1, 34) {
         println!("cargo:rustc-cfg=has_sized_atomics");
     }
+    if cfg.probe_rustc_version(1, 36) {
+        println!("cargo:rustc-cfg=has_maybe_uninit");
+    }
 }
