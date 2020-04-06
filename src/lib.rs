dce000d302198586d1dee440129c69dbf81390f1
Amanieu d'Antras <amanieu@gmail.com>
2020-04-06 21:31:19 +0100

    Use llvm_asm! instead of asm!

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@
 
 #![warn(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![cfg_attr(feature = "nightly", feature(asm))]
+#![cfg_attr(feature = "nightly", feature(llvm_asm))]
 
 mod condvar;
 mod elision;
