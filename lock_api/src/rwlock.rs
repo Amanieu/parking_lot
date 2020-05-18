0f33276132c7342ef31b01333995a3d063e5033e
Noah <33094578+coolreader18@users.noreply.github.com>
2020-05-18 13:42:13 -0500

    Add [Raw]RwLock::is_locked()

--- lock_api/src/rwlock.rs
+++ lock_api/src/rwlock.rs
@@ -56,6 +56,29 @@ pub unsafe trait RawRwLock {
 
     /// Releases an exclusive lock.
     fn unlock_exclusive(&self);
+
+    /// Checks if this `RwLock` is currently locked in any way.
+    fn is_locked(&self) -> bool {
+        self.is_locked_shared() || self.is_locked_exclusive()
+    }
+
+    /// Checks if this `RwLock` currently has shared locks.
+    fn is_locked_shared(&self) -> bool {
+        let acquired_lock = self.try_lock_shared();
+        if acquired_lock {
+            self.unlock_shared();
+        }
+        !acquired_lock
+    }
+
+    /// Checks if this `RwLock` is currently locked exclusively.
+    fn is_locked_exclusive(&self) -> bool {
+        let acquired_lock = self.try_lock_exclusive();
+        if acquired_lock {
+            self.unlock_exclusive();
+        }
+        !acquired_lock
+    }
 }
 
 /// Additional methods for RwLocks which support fair unlocking.
@@ -410,6 +433,24 @@ impl<R: RawRwLock, T: ?Sized> RwLock<R, T> {
     #[inline]
     pub fn get_mut(&mut self) -> &mut T {
         unsafe { &mut *self.data.get() }
+    }
+
+    /// Checks whether this `RwLock` is currently locked in any way.
+    #[inline]
+    pub fn is_locked(&self) -> bool {
+        self.raw.is_locked()
+    }
+
+    /// Checks whether this `RwLock` currently has any read locks.
+    #[inline]
+    pub fn is_read_locked(&self) -> bool {
+        self.raw.is_locked_shared()
+    }
+
+    /// Checks whether this `RwLock` currently has a write lock.
+    #[inline]
+    pub fn is_write_locked(&self) -> bool {
+        self.raw.is_locked_shared()
     }
 
     /// Forcibly unlocks a read lock.
