9c7c85c4c0258ee18ccbe3da98880858329eb1f3
jtnunley <jtnunley01@gmail.com>
2022-08-23 13:51:30 -0700

    Fix bounds

--- lock_api/src/mutex.rs
+++ lock_api/src/mutex.rs
@@ -681,16 +681,14 @@ unsafe impl<'a, R: RawMutex + 'a, T: ?Sized + 'a> StableAddress for MutexGuard<'
 #[must_use = "if unused the Mutex will immediately unlock"]
 pub struct ArcMutexGuard<R: RawMutex, T: ?Sized> {
     mutex: Arc<Mutex<R, T>>,
-    // n.b. notgull: add a ptr here to prevent the MutexGuard
-    // from being Sync unless it is Sync itself.
-    marker: PhantomData<(R::GuardMarker, *const ())>,
+    marker: PhantomData<*const ()>,
 }
 
-unsafe impl<R: RawMutex, T: Send + Sync + ?Sized> Send for ArcMutexGuard<R, T> where
+unsafe impl<R: RawMutex + Send + Sync, T: Send + Sync + ?Sized> Send for ArcMutexGuard<R, T> where
     R::GuardMarker: Send
 {
 }
-unsafe impl<R: RawMutex, T: Send + Sync + ?Sized> Sync for ArcMutexGuard<R, T> where
+unsafe impl<R: RawMutex + Sync, T: Sync + ?Sized> Sync for ArcMutexGuard<R, T> where
     R::GuardMarker: Sync
 {
 }
