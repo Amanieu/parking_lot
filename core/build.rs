be5489c003d1fe8f3efe701efffe357aae1b1f10
Andrey Turkin <andrey.turkin@gmail.com>
2024-06-29 23:05:59 +0300

    Remove warnings due to new lint on unknown cfgs

--- core/build.rs
+++ core/build.rs
@@ -3,6 +3,7 @@
 // scripts gets `cfg` info, even if the cfg is unstable.
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rustc-check-cfg=cfg(tsan_enabled)");
     let santizer_list = std::env::var("CARGO_CFG_SANITIZE").unwrap_or_default();
     if santizer_list.contains("thread") {
         println!("cargo:rustc-cfg=tsan_enabled");
--- lock_api/src/lib.rs
+++ lock_api/src/lib.rs
@@ -100,6 +100,7 @@ extern crate alloc;
 pub struct GuardSend(());
 
 /// Marker type which indicates that the Guard type for a lock is not `Send`.
+#[allow(dead_code)]
 pub struct GuardNoSend(*mut ());
 
 unsafe impl Sync for GuardNoSend {}
