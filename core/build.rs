69c1747db22cdf3c43007a3aee5a69a893bb6088
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-06-16 16:14:40 +0300

    Update build.rs

--- core/build.rs
+++ core/build.rs
@@ -4,8 +4,8 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-check-cfg=cfg(tsan_enabled)");
-    let santizer_list = std::env::var("CARGO_CFG_SANITIZE").unwrap_or_default();
-    if santizer_list.contains("thread") {
+    let sanitizer_list = std::env::var("CARGO_CFG_SANITIZE").unwrap_or_default();
+    if sanitizer_list.contains("thread") {
         println!("cargo:rustc-cfg=tsan_enabled");
     }
 }
